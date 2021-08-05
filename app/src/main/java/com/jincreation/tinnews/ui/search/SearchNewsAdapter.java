7e7455c80f211c4e53e658e24d28138aa45d732c
Jinfan <kimthebestman@gmail.com>
2021-08-04 23:33:57 -0700

    Search & Home(Card-stack-style-like/dislike) page completed

---
+++ app/src/main/java/com/jincreation/tinnews/ui/search/SearchNewsAdapter.java
@@ -0,0 +1,62 @@
+package com.jincreation.tinnews.ui.search;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.jincreation.tinnews.R;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jincreation.tinnews.databinding.SearchNewsItemBinding;
+import com.jincreation.tinnews.model.Article;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchNewsAdapter extends RecyclerView.Adapter<SearchNewsAdapter.SearchNewsViewHolder> {
+    private List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public SearchNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_news_item, parent, false);
+        return new SearchNewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchNewsViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.itemTitleTextView.setText(article.title);
+        Picasso.get().load(article.urlToImage).resize(200, 200).into(holder.itemImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    public static class SearchNewsViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView itemImageView;
+        TextView itemTitleTextView;
+
+        public SearchNewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            SearchNewsItemBinding binding = SearchNewsItemBinding.bind(itemView);
+            itemImageView = binding.searchItemImage;
+            itemTitleTextView = binding.searchItemTitle;
+        }
+
+    }
+}
